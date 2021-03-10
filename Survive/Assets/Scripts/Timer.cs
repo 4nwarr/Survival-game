550140f2a497f1f77e7247d38e9c6c16225e9ea7
4nwarr <drais203@gmail.com>
2021-03-10 11:21:54 +0100

    Aggiunto timer provvisorio

---
+++ Survive/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text text;
+    private int hour = 0;
+    void Start()
+    {
+        InvokeHour();
+    }
+
+    private void InvokeHour()
+    {
+        Invoke("IncreaseHour", 60);
+    }
+    private void IncreaseHour()
+    {
+        if(hour < 8)
+        {
+            hour += 1;
+            text.text = "0" + hour.ToString() + ":00";
+            InvokeHour();
+        }
+    }
+}
